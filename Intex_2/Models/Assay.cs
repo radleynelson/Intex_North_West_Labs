a8a3171b0f70a9aaa159acb3ffa48b0db1fca76f
Jameson Poulton <poulton4@byu.edu>
2018-12-05 15:03:18 -0700

    Added Models: Invoice Employee Compound Client Assay

---
+++ Intex_2/Models/Assay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex_2.Models
+{
+    [Table("Assay")]
+    public class Assay
+    {
+        [Key]
+        public int assayID { get; set; }
+        public string assayType { get; set; }
+        public Nullable<int> daysToComplete { get; set; }
+        public Nullable<double> minPriceQuote { get; set; }
+        public Nullable<double> maxPriceQuote { get; set; }
+        public string assayProtocol { get; set; }
+    }
+}
