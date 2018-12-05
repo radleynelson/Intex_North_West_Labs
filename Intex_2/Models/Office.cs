e9b35092bb3b12618872e8ab9e124713019b6952
Bronson Bradshaw <bronbrad@byu.edu>
2018-12-05 15:04:17 -0700

    added models for sample, salary, results, office, materials

---
+++ Intex_2/Models/Office.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex_2.Models
+{
+    [Table("Office")]
+    public class Office
+    {
+        [Key]
+        public int officeID { get; set; }
+
+        public string officeName { get; set; }
+
+        public string officeAddress { get; set; }
+
+        public string officeCity { get; set; }
+
+        public string officeState { get; set; }
+
+        public string officeCountry { get; set; }
+
+        public Nullable<int> officeZip { get; set; }
+    }
+}
