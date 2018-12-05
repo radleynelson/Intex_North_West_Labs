d0231c613104b63041fb48d01f3046e9c949c623
Radley Nelson <ern23@byu.edu>
2018-12-05 15:03:06 -0700

    Added models I'm responsible for

---
+++ Intex_2/Models/Work_Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex_2.Models
+{
+    [Table("Work_Order")]
+    public class Work_Order
+    {
+        [Key]
+        public int workOrderID { get; set; }
+        public int employeeID { get; set; }
+        public int clientID { get; set; }
+        public string comments { get; set; }
+        public Nullable<int> LTNumber { get; set; }
+        public Nullable<int> invoiceNo { get; set; }
+        public string workOrderStatus { get; set; }
+        public Nullable<System.DateTime> dateDue { get; set; }
+    }
+}
